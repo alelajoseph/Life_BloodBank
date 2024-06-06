78f39d39831399b098ee1f6568b210f8a70c3c32
alelajoseph <alelajoseph@gmail.com>
2024-06-06 13:46:27 -0700

    Updated

--- main/admin.py
+++ main/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from .models import Otp, Appointment
 
 # Register your models here.
-admin.site.site_header = "BloodBridge Admin"
-admin.site.site_title = "BloodBridge Admin Portal"
-admin.site.index_title = "Welcome to BloodBridge Admin Portal"
+admin.site.site_header = "Life_BloodBank Admin"
+admin.site.site_title = "Life_BloodBank Admin Portal"
+admin.site.index_title = "Welcome to Life_BloodBank Admin Portal"
 admin.site.register(Otp)
 admin.site.register(Appointment)
